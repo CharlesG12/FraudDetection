db49f36ff60f8bd74dab529f43ef09e51f3ae103
Charles <cga78@sfu.ca>
2023-03-14 16:57:08 -0700

    updated AjaxResponse

--- src/main/java/com/mastercard/fraud/utils/AjaxResponse.java
+++ src/main/java/com/mastercard/fraud/utils/AjaxResponse.java
@@ -1,2 +1,31 @@
-package com.mastercard.fraud.utils;public class AjaxResponse {
+package com.mastercard.fraud.utils;
+
+import lombok.Data;
+
+import javax.swing.*;
+
+@Data
+public class AjaxResponse {
+    private boolean isOk;
+    private int code;
+    private String message;
+    private Object data;
+
+    public static AjaxResponse success(Object obj) {
+        AjaxResponse ajaxResponse = new AjaxResponse();
+        ajaxResponse.setOk(true);
+        ajaxResponse.setCode(200);
+        ajaxResponse.setMessage("Get Successes");
+        ajaxResponse.setData(obj);
+        return ajaxResponse;
+    }
+
+    public static AjaxResponse success() {
+        AjaxResponse ajaxResponse = new AjaxResponse();
+        ajaxResponse.setOk(true);
+        ajaxResponse.setCode(200);
+        ajaxResponse.setMessage("test Successes");
+        return ajaxResponse;
+    }
+
 }
