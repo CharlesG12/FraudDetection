2e93c4204bac6f264d690649453ea31fa5316a09
Charles <cga78@sfu.ca>
2023-03-14 16:57:47 -0700

    update controller

--- src/main/java/com/mastercard/fraud/controller/TransactionAnalyzeController.java
+++ src/main/java/com/mastercard/fraud/controller/TransactionAnalyzeController.java
@@ -1,2 +1,24 @@
-package com.mastercard.fraud.controller;public class TransactionAnalyzeController {
+package com.mastercard.fraud.controller;
+
+import com.mastercard.fraud.model.request.RequestPOJO;
+import com.mastercard.fraud.utils.AjaxResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/analyzeTransaction")
+@Slf4j
+public class TransactionAnalyzeController {
+
+    @GetMapping("/test")
+    public AjaxResponse hello() {
+        return AjaxResponse.success();
+    }
+
+    @PostMapping
+    public AjaxResponse analyzeTransaction(@RequestBody RequestPOJO requestPOJO) {
+        log.info("analyze transaction post request:" + requestPOJO);
+        return AjaxResponse.success();
+    }
+
 }
