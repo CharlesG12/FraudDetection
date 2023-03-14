e60ba4d362a5c384608cdba85874f7a8b049aecc
Charles <cga78@sfu.ca>
2023-03-14 12:31:25 -0700

    initial setup, requirement doc

---
+++ src/main/java/com/mastercard/fraud/BankFraudDectionApplication.java
@@ -0,0 +1,13 @@
+package com.mastercard.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankFraudDectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankFraudDectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastercard/fraud/BankFraudDectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankFraudDectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
