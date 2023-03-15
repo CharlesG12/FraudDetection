30f63f45a3c5dce9dcd0a292d41c354d0387f760
Charles <cga78@sfu.ca>
2023-03-15 12:04:47 -0700

    add service and test

---
+++ src/main/java/com/mastercard/fraud/service/ExternalService.java
@@ -0,0 +1,74 @@
+package com.mastercard.fraud.service;
+
+import com.mastercard.fraud.model.CardUsageDto;
+import com.mastercard.fraud.model.CardUsagePO;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.Random;
+
+@Slf4j
+@Service("ExternalService")
+public class ExternalService {
+    private final String url = "http://www.randomnumberapi.com/api/v1.0/random?min=0&max=12&count=7";
+
+    @Resource
+    public WebClient webClient;
+
+    @CrossOrigin(origins = "http://www.randomnumberapi.com")
+    public CardUsageDto searchCardUsage(String cardNum) {
+        CardUsagePO[] cardUsage_list = new CardUsagePO[7];
+        try {
+            cardUsage_list = webClient
+                    .get()
+                    .uri(url)
+                    .retrieve()
+                    .bodyToMono(CardUsagePO[].class)
+                    .block();
+        } catch (Exception  ex){
+            log.error(ex.getMessage());
+
+            return dummyData(ex.getMessage());
+        }
+
+        Integer totalUsage = Arrays.stream(cardUsage_list).map(CardUsagePO::getUsage).reduce(0, Integer::sum);
+
+        CardUsageDto cardUsageDto = CardUsageDto
+                .builder()
+                .weeklyUsage(cardUsage_list)
+                .totalUsage(totalUsage)
+                .message("ok")
+                .build();
+
+        log.info("external api request:" + cardNum + " " + cardUsageDto);
+
+        return cardUsageDto;
+    }
+
+    public CardUsageDto dummyData(String message) {
+        String cardNum = "1234567890123456";
+        Integer totalUsage = 0;
+        Random rd = new Random();
+        CardUsagePO[] cardUsageList = new CardUsagePO[7];
+
+        for (int i = 0; i < cardUsageList.length; i++) {
+            Integer count = rd.nextInt(0, 20);
+            cardUsageList[i] = new CardUsagePO(count);
+            totalUsage += count;
+        }
+
+        CardUsageDto cardUsage = CardUsageDto
+                .builder()
+                .weeklyUsage(cardUsageList)
+                .totalUsage(totalUsage)
+                .message(message)
+                .build();
+
+        return cardUsage;
+    }
+
+}
---
+++ src/test/java/com/mastercard/fraud/service/ExternalServiceTest.java
@@ -0,0 +1,69 @@
+package com.mastercard.fraud.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.mastercard.fraud.model.CardUsageDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
+
+import com.mastercard.fraud.model.CardUsagePO;
+
+
+@Slf4j
+@WebMvcTest(ExternalService.class)
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class ExternalServiceTest {
+    @Mock
+    private ExternalService externalService;
+
+    @Test
+    void testSearchCardUsage() {
+        // arrange
+        String cardNum = "1234567890123456";
+        Integer totalUsage = 0;
+        CardUsagePO[] cardUsageList = new CardUsagePO[7];
+        for (int i = 0; i < cardUsageList.length; i++) {
+            cardUsageList[i] = new CardUsagePO(i+1);
+            totalUsage += i+1;
+        }
+
+        CardUsageDto expectedCardUsage = CardUsageDto
+                .builder()
+                .weeklyUsage(cardUsageList)
+                .totalUsage(totalUsage)
+                .build();
+
+        WebClient webClientMock = mock(WebClient.class);
+        ResponseSpec responseSpecMock = mock(ResponseSpec.class);
+
+        when(externalService.searchCardUsage(cardNum)).thenReturn(expectedCardUsage);
+
+//        when(responseSpecMock.bodyToMono(CardUsagePO[].class)).thenReturn(
+//                Mono.just(expectedCardUsage)
+//        );
+
+        // act
+        CardUsageDto actualCardUsage = externalService.searchCardUsage(cardNum);
+
+        log.info(actualCardUsage.toString());
+        // assert
+//        Assertions.assertArrayEquals(expectedCardUsage, actualCardUsage);
+//        assertThat(actualCardUsage.length).isNotNull();;
+    }
+
+
+
+
+}
